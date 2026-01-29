9581b578cd0387d9970c2f07b503b1fe13ca8be4
DoMinhHung <dominhhung04032003@gmail.com>
2026-01-29 12:12:16 +0700

    first commit

---
+++ src/main/java/iuh/fit/se/tramcamxuc/TramCamXucThesisApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.tramcamxuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramCamXucThesisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TramCamXucThesisApplication.class, args);
+    }
+
+}
