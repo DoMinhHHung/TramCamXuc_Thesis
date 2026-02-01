1b143e4107c71cb56f3f1ff5db401c944efe05bb
DoMinhHung <dominhhung04032003@gmail.com>
2026-02-01 11:23:35 +0700

    feat: Reorder SongStatus enum values for improved clarity and consistency

--- src/main/java/iuh/fit/se/tramcamxuc/modules/song/entity/enums/SongStatus.java
+++ src/main/java/iuh/fit/se/tramcamxuc/modules/song/entity/enums/SongStatus.java
@@ -1,11 +1,12 @@
 package iuh.fit.se.tramcamxuc.modules.song.entity.enums;
 
 public enum SongStatus {
-    DRAFT,
-    PENDING_APPROVAL,
-    PUBLIC,
-    PRIVATE,
-    REJECTED,
+    PROCESSING,          
+    DRAFT,               
+    PENDING_APPROVAL,    
+    PUBLIC,              
+    PRIVATE,             
+    REJECTED,            
     COPYRIGHT_VIOLATION,
     TRANSCODE_FAILED,
     DELETED
